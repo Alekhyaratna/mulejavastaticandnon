4380fde24f17119ca2b138b385feed34d3a7cc5d
Alekhya RATNA <alekhyar.in@mouritech.com>
2021-12-23 16:48:05 +0530

    static committed

---
+++ src/main/java/mulejavastaticandnon/staticndnonstatic.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package mulejavastaticandnon;
+
+/**
+ * @author alekhyar
+ *
+ */
+public class staticndnonstatic {
+	public static String welcome(String name) {
+
+		return "Welcome to MuleSoft "+ name;
+		}
+		public String getBalance(String bankName) {
+
+		if(bankName.equals("HDFC")) {
+		return "1000";
+		}
+
+		else if(bankName.equals("SBI")) {
+
+		return "2000";
+
+		}
+
+		else {
+
+		return "Bank doesn't exist";
+
+		}
+
+		}
+
+}
